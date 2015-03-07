f287348303987ffe5c3882bfa4c0950754831eca
James Lowrey <lowrey.46@osu.edu>
2015-03-07 13:11:26 -0500

    Swtiched to using Sass & Middleman. Started working on Index. Currently have a 3d cube that rotates with JS

---
+++ source/javascripts/cube.js
@@ -0,0 +1,37 @@
+function setTransform(element,value){
+    element.style.webkitTransform = value;
+    element.style.MozTransform = value;
+    element.style.msTransform = value;
+    element.style.OTransform = value;
+    element.style.transform = value;
+}
+
+function rotateFront(){
+    var element =  document.querySelector("._3dbox");
+    setTransform(element, "rotateY(0deg)");
+}
+
+function rotateLeft(){
+    var element =  document.querySelector("._3dbox");
+    setTransform(element, "rotateY(90deg)");
+}
+
+function rotateRight(){
+    var element =  document.querySelector("._3dbox");
+    setTransform(element, "rotateY(-90deg)");
+}
+
+function rotateBack(){
+    var element =  document.querySelector("._3dbox");
+    setTransform(element, "rotateY(180deg)");
+}
+
+function rotateTop(){
+    var element =  document.querySelector("._3dbox");
+    setTransform(element, "rotateX(-90deg)");
+}
+
+function rotateBottom(){
+    var element =  document.querySelector("._3dbox");
+    setTransform(element, "rotateX(90deg)");
+}
