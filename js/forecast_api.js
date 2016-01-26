cd3c38773ce8e4ee95f2b3402ede8cd61c84b2ae
jtronlabs <jtronlabs@gmail.com>
2016-01-25 22:46:47 -0500

    added html+css+javascript autocompletion for the weather data. Basic stlying+ icons were added

--- js/forecast_api.js
+++ js/forecast_api.js
@@ -1,5 +1,11 @@
 //API: https://developer.forecast.io/docs/v2
 
+var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+var dayNames = ["Su", "M", "Tu", "W", "Th", "F",
+  "Sa"
+];
 
 function forecast_api_call(place){
     var request = "https://api.forecast.io/forecast/2939c13b8f65b1892eefa9d96c60669b/"
@@ -25,5 +31,53 @@ function forecast_api_call(place){
 }
 
 function update_html_with_forecast_results(data){
+  $("#weather").empty();
 
+  data["daily"]["data"].forEach(get_weather_html)
+}
+
+function get_weather_html(current_element,index,array){
+      var today = new Date();
+      today.setDate(today.getDate() + index);
+      var iconName = current_element["icon"];
+      var minTmp = Math.round(current_element["temperatureMin"]);
+      var maxTmp = Math.round(current_element["temperatureMax"]);
+      var iconName = current_element["icon"];
+      var windSpd = current_element["windSpeed"];
+      var precipProbability = Math.round(current_element["precipProbability"] * 100);
+
+      /*
+      //if precip intensity is small (won't rain), don't include it
+      var precipIntensity = current_element["precipIntensity"];
+      var precipIntensityString =  ", "+precipIntensity+"in/hr";
+      if(precipIntensity < 0.01){
+        precipIntensityString = "";
+      }
+      */
+      var precipIntensityString =  "";
+
+      var html =
+      "<div class=\"weather_item\">"
+      +"    <div class=\"date\"><strong>"+dayNames[today.getDay()]+" "+monthNames[today.getMonth()]+" "+today.getDate()+", "+today.getFullYear()
+      +"      </strong></div>"
+      +"    <div class=\"weather_icon\">"
+      +"       <img src=\"img/weather_icons/"+iconName+".svg\" alt=\"rain\" >"
+      +"    </div>"
+      +"    <div class=\"weather_info\">"
+      +"      <span class=\"temp\">"
+      +"       <img src=\"img/weather_icons/thermometer.svg\" alt=\"Thermometer\" >"
+      +"       <span id=\"temp_value\">"+minTmp+" - "+maxTmp+" F</span>"
+      +"      </span>"
+      +"      <span class=\"wind_spd\">"
+      +"       <img src=\"img/weather_icons/wind_spd.svg\" alt=\"Wind\" >"
+      +"       <span id=\"wind_spd_value\">"+windSpd+" mph</span>"
+      +"      </span>"
+      +"      <span class=\"precip\">"
+      +"       <img src=\"img/weather_icons/droplet.svg\" alt=\"Precipitation\" >"
+      +"       <span id=\"precip_value\">"+precipProbability+"%"+precipIntensityString+"</span>"
+      +"      </span>"
+      +"    </div>"
+      +"</div>";
+
+      $("#weather").append(html);
 }
