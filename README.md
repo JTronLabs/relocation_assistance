579df6e4ef6ec5b4e73f3f80eaec545347ba2020
jtronlabs <jtronlabs@gmail.com>
2016-01-25 19:39:46 -0500

    initial commit to relocation website repo

--- README.md
+++
@@ -1,8 +0,0 @@
-# JTronLabs.github.io
-
-Personal website of James Lowrey, JTronlabs. Currently displays what companies Ive worked at and some of the projects I have done on my own. There are links for getting into contact with me and learning more about me. Specifically, links are provided to my profiles on LinkedIn, GitHub, Google Play, and Reddit.
-
-####MiddleMan
-use 'middleman build' to compile assets to usable css and html files
-
-use 'middleman server' to view code changes in real-time at localhost:4567
---
+++ google_places_api.js
@@ -0,0 +1,27 @@
+//requires Google Places API to be loaded into HTML file
+
+
+// This example displays an address form, using the autocomplete feature
+// of the Google Places API to help users fill in the information.
+var autocomplete;
+
+function initAutocomplete() {
+  // Create the autocomplete object, restricting the search to geographical
+  // location types.
+  autocomplete = new google.maps.places.Autocomplete(
+      (document.getElementById('autocomplete')),
+      {types: ['(cities)']}
+    );
+
+  // When the user selects an address from the dropdown, populate the address
+  // fields in the form.
+  autocomplete.addListener('place_changed', respondToPlaceSelection);
+}
+
+
+function respondToPlaceSelection() {
+  var place = autocomplete.getPlace();
+  console.log( place );
+  forecast_api_call(place);
+  //trulia_api_call(place["address_components"][0]["long_name"], place["address_components"][2]["short_name"]);//city,state
+}
